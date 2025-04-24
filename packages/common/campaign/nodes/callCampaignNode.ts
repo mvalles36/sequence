f89a66f327de5c0c8f3850c2d55f00d2f5716e4c
Mark Valles <mvalles36@users.noreply.github.com>
2025-04-24 02:21:44 -0500

    Update callCampaignNode.ts

--- packages/common/campaign/nodes/callCampaignNode.ts
+++ packages/common/campaign/nodes/callCampaignNode.ts
@@ -1,1 +1,54 @@
-blank
+// packages/common/campaign/nodes/callCampaignNode.ts
+import AbstractCampaignNode from "./abstractCampaignNode";
+import { CampaignNodeKind } from "../types";
+
+/**
+ * Represents an outbound call node in a campaign
+ */
+class CallCampaignNode extends AbstractCampaignNode {
+  kind = CampaignNodeKind.OutboundCall;
+  #callScriptId?: string;
+  #schedulingType?: string;
+  #callerId?: string;
+  
+  private constructor() {
+    super();
+  }
+
+  static new() {
+    return new CallCampaignNode();
+  }
+
+  toString() {
+    return `Call: ${this.name || this.id}`;
+  }
+
+  setCallScriptId(value?: string) {
+    this.#callScriptId = value;
+    return this;
+  }
+
+  getCallScriptId() {
+    return this.#callScriptId;
+  }
+
+  setSchedulingType(value?: string) {
+    this.#schedulingType = value;
+    return this;
+  }
+
+  getSchedulingType() {
+    return this.#schedulingType;
+  }
+
+  setCallerId(value?: string) {
+    this.#callerId = value;
+    return this;
+  }
+
+  getCallerId() {
+    return this.#callerId;
+  }
+}
+
+export default CallCampaignNode;
